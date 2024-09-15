c298630c727447dfca9466f6f693c7bccdaecb96
Krzysztof Radomski <krzysztof.radomski@netigate.se>
2024-09-15 23:56:57 +0200

    add cors handling to server

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import WebSocket from "ws";
+import cors from "cors";
 import { handleConnection } from "./src/server/connection/handleConnection";
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// if (process.env.NODE_ENV === "development") {
+app.use(cors());
+// }
 
 app.use(express.static("public"));
 
